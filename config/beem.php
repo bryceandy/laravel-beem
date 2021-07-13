5e46afdbd4719e40b7e78273fb9fda43565e5242
bryceandy <bryceandy@rocketmail.com>
2021-07-13 20:58:12 +0300

    Create the packages config file

---
+++ config/beem.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Beem API Key
+    |--------------------------------------------------------------------------
+    |
+    | You can obtain this key after creating a Beem vendor account, then
+    | visit the Profile tab and click on "Authentication Information"
+    |
+    */
+    'api_key' => env('BEEM_KEY'),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Beem Secret Key
+   |--------------------------------------------------------------------------
+   |
+   | You can obtain this key after creating a Beem vendor account, then visit
+   | the Profile tab and click on "Authentication Information" then Generate
+   |
+   */
+    'secret_key' => env('BEEM_SECRET'),
+
+];
