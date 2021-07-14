bd7ec6166e345801d6dd1451c7cdf3e04a52ab97
bryceandy <bryceandy@rocketmail.com>
2021-07-14 17:56:03 +0300

    Send SMS with schedule

--- src/Beem.php
+++ src/Beem.php
@@ -3,6 +3,7 @@
 namespace Bryceandy\Beem;
 
 use Bryceandy\Beem\Traits\MakesHttpRequests;
+use Carbon\Carbon;
 use Illuminate\Http\Client\Response;
 
 class Beem
@@ -25,6 +26,35 @@ class Beem
             'POST',
             array_merge(
                 ['encoding' => 0],
+                compact('message', 'recipients', 'source_addr')
+            )
+        );
+    }
+
+    /**
+     * @param string $message
+     * @param array $recipients
+     * @param string $schedule
+     * @param string $source_addr
+     *
+     * @return Response
+     *
+     * @throws Exceptions\ConfigurationUnavailableException
+     */
+    public function smsWithSchedule(
+        string $message,
+        array $recipients,
+        string $schedule,
+        string $source_addr = 'INFO'
+    ): Response
+    {
+        $scheduledTime = Carbon::parse($schedule)->format('Y-m-d H:m');
+
+        return $this->call(
+            'https://apisms.beem.africa/v1/send',
+            'POST',
+            array_merge(
+                ['encoding' => 0, 'schedule_time' => $scheduledTime],
                 compact('message', 'recipients', 'source_addr')
             )
         );
