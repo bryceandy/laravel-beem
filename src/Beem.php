cc91de92c982f34115bb2fcca4e93473cf67deef
bryceandy <bryceandy@rocketmail.com>
2021-07-14 18:37:28 +0300

    Refactor sms requests

--- src/Beem.php
+++ src/Beem.php
@@ -2,74 +2,9 @@
 
 namespace Bryceandy\Beem;
 
-use Bryceandy\Beem\Traits\MakesHttpRequests;
-use Carbon\Carbon;
-use Illuminate\Http\Client\Response;
+use Bryceandy\Beem\Traits\Sms\HandlesSms;
 
 class Beem
 {
-    use MakesHttpRequests;
-
-    /**
-     * @param string $message
-     * @param array $recipients
-     * @param string $source_addr
-     *
-     * @return Response
-     *
-     * @throws Exceptions\ConfigurationUnavailableException
-     */
-    public function sms(string $message, array $recipients, string $source_addr = 'INFO'): Response
-    {
-        return $this->call(
-            'https://apisms.beem.africa/v1/send',
-            'POST',
-            array_merge(
-                ['encoding' => 0],
-                compact('message', 'recipients', 'source_addr')
-            )
-        );
-    }
-
-    /**
-     * @param string $message
-     * @param array $recipients
-     * @param string $schedule
-     * @param string $source_addr
-     *
-     * @return Response
-     *
-     * @throws Exceptions\ConfigurationUnavailableException
-     */
-    public function smsWithSchedule(
-        string $message,
-        array $recipients,
-        string $schedule,
-        string $source_addr = 'INFO'
-    ): Response
-    {
-        $scheduledTime = Carbon::parse($schedule)->format('Y-m-d H:m');
-
-        return $this->call(
-            'https://apisms.beem.africa/v1/send',
-            'POST',
-            array_merge(
-                ['encoding' => 0, 'schedule_time' => $scheduledTime],
-                compact('message', 'recipients', 'source_addr')
-            )
-        );
-    }
-
-    /**
-     * @return Response
-     *
-     * @throws Exceptions\ConfigurationUnavailableException
-     */
-    public function smsBalance(): Response
-    {
-        return $this->call(
-            'https://apisms.beem.africa/public/v1/vendors/balance',
-            'GET'
-        );
-    }
+    use HandlesSms;
 }
---
+++ src/Traits/Sms/HandlesSms.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Bryceandy\Beem\Traits\Sms;
+
+use Bryceandy\Beem\Exceptions\ConfigurationUnavailableException;
+use Bryceandy\Beem\Traits\MakesHttpRequests;
+use Carbon\Carbon;
+use Illuminate\Http\Client\Response;
+
+trait HandlesSms
+{
+    use MakesHttpRequests;
+
+    /**
+     * @param string $message
+     * @param array $recipients
+     * @param string $source_addr
+     *
+     * @return Response
+     *
+     * @throws ConfigurationUnavailableException
+     */
+    public function sms(string $message, array $recipients, string $source_addr = 'INFO'): Response
+    {
+        return $this->call(
+            'https://apisms.beem.africa/v1/send',
+            'POST',
+            array_merge(
+                ['encoding' => 0],
+                compact('message', 'recipients', 'source_addr')
+            )
+        );
+    }
+
+    /**
+     * @param string $message
+     * @param array $recipients
+     * @param string $schedule
+     * @param string $source_addr
+     *
+     * @return Response
+     *
+     * @throws ConfigurationUnavailableException
+     */
+    public function smsWithSchedule(
+        string $message,
+        array $recipients,
+        string $schedule,
+        string $source_addr = 'INFO'
+    ): Response
+    {
+        $scheduledTime = Carbon::parse($schedule)->format('Y-m-d H:m');
+
+        return $this->call(
+            'https://apisms.beem.africa/v1/send',
+            'POST',
+            array_merge(
+                ['encoding' => 0, 'schedule_time' => $scheduledTime],
+                compact('message', 'recipients', 'source_addr')
+            )
+        );
+    }
+
+    /**
+     * @return Response
+     *
+     * @throws ConfigurationUnavailableException
+     */
+    public function smsBalance(): Response
+    {
+        return $this->call(
+            'https://apisms.beem.africa/public/v1/vendors/balance',
+            'GET'
+        );
+    }
+}
