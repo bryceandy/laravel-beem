c5f2b962e8c24a682bcc50214cd8a97174555625
bryceandy <bryceandy@rocketmail.com>
2021-07-15 21:31:57 +0300

    Create a route for outbound sms

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Bryceandy\Beem\Http\Controllers\TwoWaySmsCallbackController;
 use Bryceandy\Beem\Http\Controllers\UssdCallbackController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('outbound-sms', TwoWaySmsCallbackController::class)
+    ->name('beem.outbound-sms');
 
 Route::post('ussd-callback', UssdCallbackController::class)
     ->name('beem.ussd-callback');
