b45bac2960c14e151897ffcac9eca4aef7386005
bryceandy <bryceandy@rocketmail.com>
2021-07-15 18:36:34 +0300

    Dispatch event from the action

--- src/Http/Controllers/UssdCallbackController.php
+++ src/Http/Controllers/UssdCallbackController.php
@@ -2,13 +2,17 @@
 
 namespace Bryceandy\Beem\Http\Controllers;
 
+use Bryceandy\Beem\Events\UssdCallbackReceived;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class UssdCallbackController extends Controller
 {
+    /**
+     * @param Request $request
+     */
     public function __invoke(Request $request)
     {
-
+        UssdCallbackReceived::dispatch($request);
     }
 }
