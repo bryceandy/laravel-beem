4a1c21ad50f997c1fe61c512828ea09fdd39663c
bryceandy <bryceandy@rocketmail.com>
2021-07-15 18:26:47 +0300

    Create controller to receive callback data

---
+++ src/Http/Controllers/UssdCallbackController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bryceandy\Beem\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class UssdCallbackController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+
+    }
+}
