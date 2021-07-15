6882bdd9691d4e86078d02cd1a3226c3316777d4
bryceandy <bryceandy@rocketmail.com>
2021-07-15 17:34:18 +0300

    Add name to route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Bryceandy\Beem\Http\Controllers\UssdCallbackController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('beem-ussd-callback', UssdCallbackController::class);
+Route::post('beem-ussd-callback', UssdCallbackController::class)
+    ->name('beem.ussd.callback');
